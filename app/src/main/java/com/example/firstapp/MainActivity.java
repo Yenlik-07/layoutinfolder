b7287a4a6a76424897f035739190ba36a74aead7
Your Name <you@example.com>
2020-10-31 13:49:38 +0600

    first commit

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.firstapp.R;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btnClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.button_on_click);
+
+        textView = findViewById(R.id.tvText);
+        btnClicked = findViewById(R.id.btn);
+    }
+
+
+    public void btn(View view) {
+        textView.setText("Successfully");
     }
 }
